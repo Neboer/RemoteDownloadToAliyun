43c036e990c3fb3ed679ea9bcf5f93eed6933c9b
Neboer <43609792+Neboer@users.noreply.github.com>
2022-03-26 17:58:55 +0800

    youtube-dl

--- prepare.sh
+++ prepare.sh
@@ -11,9 +11,7 @@ sudo python -m pip install git+https://github.com/wxy1343/aliyunpan.git
 mkdir -p ~/.config && echo "refresh_token: '$REFRESH_TOKEN'"  >  ~/.config/aliyunpan.yaml
 if [[ "$1" == youtube-dl* ]]
 then
-sudo add-apt-repository ppa:nilarimogard/webupd8
-sudo apt-get update
-sudo apt install -y youtube-dl
+sudo python -m pip install youtube-dl
 else
 sudo apt install -y aria2c
 fi
