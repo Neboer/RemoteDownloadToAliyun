935cd2465495d208e4fc90d0ba1606c6c474a8ad
Neboer <43609792+Neboer@users.noreply.github.com>
2022-03-26 15:53:04 +0800

    fix error

--- prepare.sh
+++ prepare.sh
@@ -5,11 +5,14 @@
 #  REFRESH_TOKEN: ${{ secrets.REFRESH_TOKEN }}
 
 # prepare environment
+sudo apt-get update
 sudo apt install -y python3
 sudo python -m pip install git+https://github.com/wxy1343/aliyunpan.git
 mkdir -p ~/.config && echo "refresh_token: '$REFRESH_TOKEN'"  >  ~/.config/aliyunpan.yaml
 if [[ $1 == youtube-dl* ]]
 then
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
 sudo apt install -y youtube-dl
 else
 sudo apt install -y aria2c
